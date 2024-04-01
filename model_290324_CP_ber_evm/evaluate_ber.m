f5b23bd987292488c9108f59505bdcaf47e691de
Me_working_computer <ilya.voropaev@metalab.ifmo.ru>
2024-04-01 19:31:45 +0300

    bugs improved

--- model_290324_CP_ber_evm/evaluate_ber.m
+++ model_290324_CP_ber_evm/evaluate_ber.m
@@ -11,13 +11,13 @@ function ber = evaluate_ber(original_message, decoded_message, M)
     num_bits = 0;
 
     for k=1:length(original_message)
-        vec1 = str2num(char(num2cell(bin_original_message(k))));
-        vec2 = str2num(char(num2cell(bin_decoded_message(k))));
+        vec1 = str2num(char(num2cell(bin_original_message(k, :))));
+        vec2 = str2num(char(num2cell(bin_decoded_message(k, :))));
         sum_error = sum_error + sum(abs(vec1-vec2));
         num_bits = num_bits + length(vec1);
     end
     ber = sum_error / num_bits;
 end
 
-% 29.03.24.
-% conversion from integer to binary updated; an error improved
+% 1.04.24.
+% a bug with vec1 vec2 improved
